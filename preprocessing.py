27688b136e83675b7d94dbf22a4ba0fe7105b302
Adamghosam <adamghosam@gmail.com>
2023-03-20 02:31:40 +0700

    menganti nama data covid Ke data.csv dan menambhkan procesing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,24 @@
-import numpy 
+# Nama : Adam Ghosam
+# Nim : A11.2019.12040
+# Title : Data Mining Covid 19 di indonesia
+
+# import library untuk Proses data Sient
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime
+
+
+# memanggil dataset
+dataset = pd.read_csv('Data.csv')
+# dataset['Month'] = dataset['Date'].apply(lambda x:datetime.datetime.strptime(x, '%m/%d/%Y').strftime('%Y-%m'))
+
+# cov = dataset[dataset['Location'] != 'Indonesia']
+
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print(x)
+print(y)
+
+
